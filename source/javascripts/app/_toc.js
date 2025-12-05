94ffa637c041bb5816c9f13e6dffa274a6a0444e
Daz Greer <daz@darrengreer.co.uk>
2025-12-05 11:04:03 +0100

    feature(documentation): style and accessibility updates
    - Upgraded Ruby version to 2.4.2
    - Gemfile dependency updates to resolve compilation and compatibility issues:
    - Updated HTML of TOC navigation for accessibility
    - Added lang and dir on html tag for accessibility
    - Updated meta tags for viewport and added description
    - Various accessibility updates via CSS for font-size, color contrast etc
    - Updated logo and added alt tag
    - Added node_modules to .gitignore

--- source/javascripts/app/_toc.js
+++ source/javascripts/app/_toc.js
@@ -1,15 +1,15 @@
 //= require ../lib/_jquery
 //= require ../lib/_imagesloaded.min
-;(function () {
-  'use strict';
+(function () {
+  "use strict";
 
   var loaded = false;
 
-  var debounce = function(func, waitTime) {
+  var debounce = function (func, waitTime) {
     var timeout = false;
-    return function() {
+    return function () {
       if (timeout === false) {
-        setTimeout(function() {
+        setTimeout(function () {
           func();
           timeout = false;
         }, waitTime);
@@ -18,9 +18,9 @@
     };
   };
 
-  var closeToc = function() {
-    $(".toc-wrapper").removeClass('open');
-    $("#nav-button").removeClass('open');
+  var closeToc = function () {
+    $(".toc-wrapper").removeClass("open");
+    $("#nav-button").removeClass("open");
   };
 
   function loadToc($toc, tocLinkSelector, tocListSelector, scrollOffset) {
@@ -29,20 +29,20 @@
     var windowHeight = 0;
     var originalTitle = document.title;
 
-    var recacheHeights = function() {
+    var recacheHeights = function () {
       headerHeights = {};
       pageHeight = $(document).height();
       windowHeight = $(window).height();
 
-      $toc.find(tocLinkSelector).each(function() {
-        var targetId = $(this).attr('href');
+      $toc.find(tocLinkSelector).each(function () {
+        var targetId = $(this).attr("href");
         if (targetId[0] === "#") {
           headerHeights[targetId] = $(targetId).offset().top;
         }
       });
     };
 
-    var refreshToc = function() {
+    var refreshToc = function () {
       var currentTop = $(document).scrollTop() + scrollOffset;
 
       if (currentTop + windowHeight >= pageHeight) {
@@ -54,7 +54,11 @@
 
       var best = null;
       for (var name in headerHeights) {
-        if ((headerHeights[name] < currentTop && headerHeights[name] > headerHeights[best]) || best === null) {
+        if (
+          (headerHeights[name] < currentTop &&
+            headerHeights[name] > headerHeights[best]) ||
+          best === null
+        ) {
           best = name;
         }
       }
@@ -72,30 +76,38 @@
         $toc.find(".active").removeClass("active");
         $toc.find(".active-parent").removeClass("active-parent");
         $best.addClass("active");
-        $best.parents(tocListSelector).addClass("active").siblings(tocLinkSelector).addClass('active-parent');
+        $best
+          .parents(tocListSelector)
+          .addClass("active")
+          .siblings(tocLinkSelector)
+          .addClass("active-parent");
         $best.siblings(tocListSelector).addClass("active");
         $toc.find(tocListSelector).filter(":not(.active)").slideUp(150);
         $toc.find(tocListSelector).filter(".active").slideDown(150);
+
+        // Add this line to update the URL
+        history.replaceState(null, "", best);
+
         // TODO remove classnames
         document.title = $best.data("title") + " – " + originalTitle;
       }
     };
 
-    var makeToc = function() {
+    var makeToc = function () {
       recacheHeights();
       refreshToc();
 
-      $("#nav-button").click(function() {
-        $(".toc-wrapper").toggleClass('open');
-        $("#nav-button").toggleClass('open');
+      $("#nav-button").click(function () {
+        $(".toc-wrapper").toggleClass("open");
+        $("#nav-button").toggleClass("open");
         return false;
       });
       $(".page-wrapper").click(closeToc);
       $(".toc-link").click(closeToc);
 
       // reload immediately after scrolling on toc click
-      $toc.find(tocLinkSelector).click(function() {
-        setTimeout(function() {
+      $toc.find(tocLinkSelector).click(function () {
+        setTimeout(function () {
           refreshToc();
         }, 0);
       });
